98b0d27bb03295ee3fff9ee954104da87027716c
kalpeshkhare7777 <132674344+kalpeshkhare7777@users.noreply.github.com>
2024-10-18 02:48:20 +0530

    Update main.js

--- turf-backend/model/main.js
+++ turf-backend/model/main.js
@@ -2,31 +2,29 @@ const mongoose = require("mongoose");
 
 const statusEnum = ['pending', 'accepted', 'declined'];
 
-const MainSchema =new mongoose.Schema(
-    {
-        rollno:{
-            type: String,
-            trim: true
-        },
-        status: {
-            type: String,
-            enum: statusEnum,
-            default: 'pending' 
-        },
-        slotno: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            expires: '7d',  
-        },      
+const MainSchema = new mongoose.Schema({
+    rollno: {
+        type: String,
+        trim: true
     },
-    {
-        timestamps: true,
-    }
-);
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'declined'],
+        default: 'pending'  // Set default status to 'pending'
+    },
+    slotno: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: '7d',
+    },
+}, {
+    timestamps: true,
+});
+
 
 const MainInfo = mongoose.model('MainInfo',MainSchema);
 
